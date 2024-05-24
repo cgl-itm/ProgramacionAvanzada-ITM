ef065b9a32e114d41fc07d90868d6bff796665d4
Cristian Guarnizo Lemus - ITM <124705294+cgl-itm@users.noreply.github.com>
2024-05-24 09:21:08 -0500

    Add files via upload

--- Dashboard/StreamSync_Ejemplos/fechaDatos/main.py
+++ Dashboard/StreamSync_Ejemplos/fechaDatos/main.py
@@ -12,7 +12,16 @@ def update(state):
     fig = px.line(df, x=df.index, y="L06_347")
     state["grafico"] = fig
     
-    
+def updateYear(state):
+    df = data.loc[state["selYear"]]
+    fig = px.line(df, x=df.index, y="L06_347")
+    state["graficoYear"] = fig
+
+def updateBox(state):
+    df = data[data.index.month == int(state['selMonth'])]
+    state["graficoBox"] = px.box(df, x=df.index.year, y="L06_347")
+
+
 # Initialise the state
 initial_state = ss.init_state({
     "my_app": {
@@ -22,6 +31,14 @@ initial_state = ss.init_state({
     "fecha_ini": data.index.min().date(),
     "fecha_fin": data.index.max().date(),
     "grafico": None,
+    "selYear": '2009',
+    "yearsInData": {str(key):str(key) for key in data.index.year.unique()},    
+    "graficoYear": None,
+    "monthsInData": {str(k):str(k) for k in range(1,13)},
+    "selMonth": '1',
+    "graficoBox": None,
 })
 
 update(initial_state)
+updateYear(initial_state)
+updateBox(initial_state)
